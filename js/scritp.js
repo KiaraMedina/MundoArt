f6e0b0c7ae149d5c84d5ae8e6f6297dd44c8a403
KiaraMedina <0332171016@unjfsc.edu.pe>
2020-11-01 16:05:31 -0500

    first commit

---
+++ js/scritp.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+    $("#myBtn").click(function() {
+        $("#myModal").modal();
+    });
+});
+
+$(document).ready(function() {
+    var $easyzoom = $('.easyzoom').easyZoom();
+});
+
+$(document).ready(function() {
+    $("#myBtn").click(function() {
+        $("#myModal1").modal();
+    });
+});
+$(document).ready(function() {
+    $("#myBtn").click(function() {
+        $("#myModal2").modal();
+    });
+});
+$(document).ready(function() {
+    $("#myBtn").click(function() {
+        $("#myModal3").modal();
+    });
+});
